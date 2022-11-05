a330006ffbd01bceeaece49b89e48d2ba0ed63c4
satrio priambodo <satriopriambodo96@gmail.com>
2022-11-05 20:20:55 +0700

    use cors

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const route = require("./routes/index");
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
